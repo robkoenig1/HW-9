aceab757054207c81373903079be4157543bd742
Robert Koenig <rkoenig@udel.edu>
2024-11-05 19:26:16 -0500

    HW-9 rev16

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -24,10 +24,10 @@ export class MainComponent extends WebzComponent {
 
     constructor() {
         super(html, css);
-        const testPixel = new PixelComponent(0, 0);
-        testPixel.setColor(new Color(255, 0, 0)); // Red
-        testPixel.setSize(50); // 50px by 50px
-        this.addComponent(testPixel);
+        //const testPixel = new PixelComponent(0, 0);
+        //testPixel.setColor(new Color(255, 0, 0)); // Red
+        //testPixel.setSize(50); // 50px by 50px
+        //this.addComponent(testPixel);
         const preview = new GridComponent(1, 32);
         this.addComponent(preview);
         preview.loadImage(this.DEFAULT_IMAGE);
